6121e3e6d05b77ab963fcfc567ce80d25bddeb43
Edison-choo <chooedison@yahoo.com>
2021-05-13 15:47:09 +0800

    add a bit

---
+++ models/menuSpecification.js
@@ -0,0 +1,7 @@
+var specifications = {
+    "spice level": ["Very spicy", "spicy", "no spice"],
+    "temperature": ["Cold", "Hot"],
+    "portion": ["Large", "Medium", "Small"],
+};
+
+module.exports = specifications;
