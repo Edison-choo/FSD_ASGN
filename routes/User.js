89f8f58a33c818103c4a4ffeeae431b88e6a6ead
Cruz Chua <cruzchua@cruzs-mbp.lan>
2021-05-14 21:11:42 +0800

    Cruz's

--- routes/User.js
+++ routes/User.js
@@ -9,4 +9,8 @@ router.get('/register', (req, res) => {
 	res.render('user/register');
 });
 
+
+ router.get('/forget_password', (req, res) => {
+	 res.render('user/forgetpassword');
+ })
 module.exports = router;
