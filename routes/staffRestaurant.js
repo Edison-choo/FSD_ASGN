1fa9c57456e138e4e5de2586c4a54f08255b87cd
WyeK <83228783+WyeK@users.noreply.github.com>
2021-05-15 21:41:47 +0800

    tried to do post on restaurant

--- routes/staffRestaurant.js
+++ routes/staffRestaurant.js
@@ -1,6 +1,7 @@
 const express = require('express');
+const Restaurants = require('../models/restaurants');
 const router = express.Router();
-
+const Restaurant = require("../models/restaurants");
 
 //Page if restaurant page not created
 router.get('/', (req, res) => {
@@ -12,4 +13,19 @@ router.get('/createRestaurant', (req, res) => {
     res.render('staffRestaurant/createRestaurant')
 });
 
+//Post for Create Restaurant Page
+router.post('/createRestaurant', (req, res) => {
+    let errors = [];
+    // let {comp_name, address, comp_email, uen, res_name, cuisine, open_time, close_time, halal, facebook, twitter, instagram} = req.body;
+    // if (errors.length > 0) {
+    //     res.render('staffRestaurant/createRestaurant')
+    // }
+    // else{
+    //     Restaurant.create({ comp_name, address, comp_email, uen, res_name, cuisine, open_time, close_time, halal, facebook, twitter, instagram })
+    //     .then(restaurant => {
+    //         console.log(res,"success", restaurant.comp_name + "added");
+    //     })
+    // }
+    console.log(req.body);
+});
 module.exports = router
