f43ea580f8cb920cf116059ed01be667a926e79b
Xu4nW31 <joshualim2122@gmail.com>
2021-05-17 22:44:43 +0800

    createPromotion & createReview changes

--- routes/createPromotions.js
+++ routes/createPromotions.js
@@ -1,8 +1,58 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const router = express.Router();
+var bodyParser = require('body-parser');
+const alertMessage = require('../helpers/messenger');
+const Promotions = require('../models/promotions');
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 router.get('/', (req, res) => {
     res.render('promotion/createPromotions');
+});
+
+router.post('/createPromotions', urlencodedParser,(req, res) => {
+    let errors = [];
+
+    let{name, startdate, enddate, discount, details, banner} = req.body;
+
+    if(errors.length > 0){
+        res.render('promotion/createPromotions', {
+            errors,
+            name,
+            startdate,
+            enddate,
+            discount,
+            details,
+            banner
+        });
+    } else{
+        Promotions.findOne({ where: {name:req.body.name} })
+        .then(promotions => {
+            if(promotions){
+                res.render('promotion/createPromotion', {
+                    error: promotions.name + 'already exist',
+                    name,
+                    startdate,
+                    enddate,
+                    discount,
+                    details,
+                    banner
+                });
+            } else {
+                Promotions.create({
+                    PromoName: name,
+                    PromoStart: startdate,
+                    PromoEnd: enddate,
+                    DiscountAmt: discount,
+                    BannerImg: banner
+                }).then(promotions =>{
+                    res.redirect('/promotion/createPromotions');
+                })
+                .catch(err => console.log(err));
+            }
+        });
+    }
 });
 
 module.exports = router;
--- routes/createReviews.js
+++ routes/createReviews.js
@@ -1,8 +1,54 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const router = express.Router();
+var bodyParser = require('body-parser');
+const alertMessage = require('../helpers/messenger');
+const Reviews = require('../models/reviews');
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 router.get('/', (req, res) => {
     res.render('reviews/createReviews');
 });
 
+router.post('/reviews/createReviews', urlencodedParser,(req,res) => {
+    let errors = [];
+
+    let{email, FoodOption, CustOption, EnvOption, comments} = req.body;
+
+    if(errors.length > 0){
+        res.render('reviews/createReviews', {
+            email,
+            FoodOption,
+            CustOption, 
+            EnvOption, 
+            comments
+        });
+    } else{
+        Reviews.findOne({ where: {email: req.body.email} })
+        .then(reviews => {
+            if (reviews){
+                res.render('/review/createReviews', {
+                    error: reviews.name + 'already exist',
+                    email,
+                    FoodOption,
+                    CustOption,
+                    EnvOption,
+                    comments
+                });
+            } else{
+                Reviews.create({
+                    EmailAddress: email,
+                    FoodRatings: FoodOption,
+                    ServiceRatings: CustOption,
+                    EnvRatings: EnvOption,
+                    AddComments: comments
+                }).then(reviews => {
+                    res.redirect('/')
+                })
+                .catch(err => console.log(err));
+            }
+        });
+    }
+});
 module.exports = router;
