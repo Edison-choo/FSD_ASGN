ef72b11bc228b4821711d181f82105eee43e25fa
Cruz Chua <cruzchua@cruzs-mbp.lan>
2021-05-18 19:52:10 +0800

    Cruz's edit

--- routes/main.js
+++ routes/main.js
@@ -2,6 +2,8 @@ const express = require('express');
 const { session } = require('passport');
 const router = express.Router();
 const User = require("../models/user");
+// login
+var validator = require("validator");
 
 var userLog = false;
 var location = "";
@@ -9,8 +11,18 @@ var location = "";
 //Home page
 router.get('/', (req, res) => {
 	const title = "FooDecent Home";
-	location = "index"
-	checkCurrentUser(req.session.passport.user, location, title, res)
+
+	location = "index";
+	
+	if(req.session.passport.user === undefined){
+		nocurrentUser(location, title, res);
+	}else{
+		checkCurrentUser(req.session.passport.user, location, title, res);
+	}
+	
+
+	
+	
 });
 
 //Booking page
@@ -23,7 +35,7 @@ router.get('/', (req, res) => {
 // Rendering of customers
 
 function checkUser(user){
-	if(user != 0){
+	if(user > 0){
 		userLog = true;
 	}else{
 		userLog = false;
@@ -31,15 +43,17 @@ function checkUser(user){
 	return userLog;
 }
 
+function nocurrentUser(location, title, res){
+	res.render(location, {title:title, userLog:false});
+}
+
 function checkCurrentUser(user, location, title, res){
 	if(user != 0){
-		User.findOne({ where: {id: user} })
-					.then(users => {
-						res.render(location, {title: title, fname: users.fname, userLog:checkUser(user)});
-					});	
-	}else{
-		res.render(location, {title:title});
-	}
+		 User.findOne({ where: {id: user} })
+		 			.then(users => {
+		 				res.render(location, {title: title, fname: users.fname, userLog:checkUser(user)});
+		 			});	
+	 }
 }
 
 module.exports = router;
