12e7efb0c894dc6113b758edef9e74107bda0c92
Edison-choo <chooedison@yahoo.com>
2021-05-19 21:42:23 +0800

    add error

--- routes/book.js
+++ routes/book.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const bodyParser = require('body-parser');
+const alertMessage = require('../helpers/messenger');
+const Menu = require('../models/menu');
+const menuSpecification = require('../models/menuSpecification');
 
 router.get('/menuBook', (req, res) => {
-	res.render('book/menuBook');
+	var types = [];
+	Menu.findAll({
+		attributes: { exclude: ['restaurantMenuId']}
+	})
+	.then(menus => {
+		if (menus) {
+			// menus.specifications = JSON.parse(menus.specifications);
+			menus.forEach((menu) => {
+				menu.specifications = JSON.parse(menu.specifications);
+				if (types.includes(menu.type) === false) {
+					types.push(menu.type);
+				}
+			});
+			types.sort();
+			res.render('book/menuBook', {menus, types, menuSpecification});
+		}
+	}).catch(err => console.log(err));
 });
 
 router.get('/foodCart', (req, res) => {
