1a863d323afe20f38161ce3194a1203e54ee99fe
Edison-choo <chooedison@yahoo.com>
2021-05-24 22:23:09 +0800

    try delete session

--- routes/book.js
+++ routes/book.js
@@ -12,7 +12,7 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 router.get("/menuBook", (req, res) => {
   var types = [];
-//   req.session.cart = undefined;
+  // req.session.cart = undefined;
   Menu.findAll({
     attributes: { exclude: ["restaurantId"] },
   })
@@ -32,21 +32,25 @@ router.get("/menuBook", (req, res) => {
 });
 
 router.get("/foodCart", (req, res) => {
-  sess = req.session;
-  let cart = sess.cart.map((c) => parseInt(c.id));
-  console.log(cart);
-  Menu.findAll({
-    where: {
-      id: { [Op.in] : cart }
-    },
-  })
-    .then((menus) => {
-      if (menus) {
-		  console.log(menus);
-        res.render("book/foodCart", {menus});
-      }
+  let sess = req.session;
+  if (sess.cart) {
+    let cart = sess.cart.map((c) => parseInt(c.id));
+
+    Menu.findAll({
+      where: {
+        id: { [Op.in] : cart }
+      },
     })
-    .catch((err) => console.log(err))
+      .then((menus) => {
+        if (menus) {
+          res.render("book/foodCart", {menus});
+        }
+      })
+      .catch((err) => console.log(err))
+  } else {
+    res.render("book/foodCart");
+  }
+  
 });
 
 router.get("/receipt", (req, res) => {
@@ -62,7 +66,7 @@ router.post("/add/:id", urlencodedParser, (req, res) => {
   cart = [];
   let { quantity, specifications } = req.body;
   // console.log(quantity, specifications)
-  sess = req.session;
+  let sess = req.session;
   cart.push({
     id: req.params.id,
     quantity: quantity,
@@ -75,6 +79,18 @@ router.post("/add/:id", urlencodedParser, (req, res) => {
   }
   console.log(sess.cart);
   res.redirect("/book/menuBook");
+});
+
+//delete item from session
+router.get('/delete/:id', (res, req) => {
+  let sess = req.session;
+  console.log(sess);
+  let cart = sess.cart.filter((c) => c.id == req.params.id);
+  if (cart) {
+    delete sess.cart[sess.cart.indexOf(cart)];
+    res.redirect('video/foodCart');
+  }
+  res.redirect('video/foodCart');
 });
 
 module.exports = router;
