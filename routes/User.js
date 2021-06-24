fdd7e89ae620688810f7d35da7123f1df493875b
Cruz Chua <cruzchua@cruzs-mbp.lan>
2021-06-24 14:21:11 +0800

    Cruz's edit

--- routes/User.js
+++ routes/User.js
@@ -6,7 +6,7 @@ const User = require("../models/user");
 const bcrypt = require("bcryptjs")
 const passport = require("passport");
 const { response } = require('express');
-
+var validator = require('email-validator');
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 
 router.get('/login', (req, res) => {
@@ -29,7 +29,7 @@ router.post('/registerUser', urlencodedParser, (req, res) => {
 	let phone = req.body.phone;
 	let usertype = "customer";
 
-	var validator = require('email-validator');
+	
 
     if(password != cpassword){
         errors.push({"text": "Password do not match"});
@@ -58,8 +58,7 @@ router.post('/registerUser', urlencodedParser, (req, res) => {
         User.findOne({ where: {email: req.body.email} })
 		.then(user => {
 			if (user) {
-		// If user is found, that means email has already been
- 		// registered
+		// If user is found, email has already been registered
 			res.render('user/register', {error: user.email + ' already registered', fname, lname, phone, email, password, cpassword });
 			} else {
  			// Create new user record
@@ -93,15 +92,66 @@ router.get('/logout', (req, res) => {
 	req.flash("success_msg", "You are logged out");
 
 	res.redirect("/user/login");
-})
+});
 
 router.get('/forget_password', (req, res) => {
 	 res.render('user/forgetpassword');
 });
 
+router.post('/changePassword', urlencodedParser, (req, res) => {
+	let errors = [];
+	let success_msg = '';
+	let {email, password, cpassword} = req.body;
+
+	emailValidate = validator.validate(email);
+	if(!emailValidate){
+		errors.push({"text": "Email is not valid"});
+	}
+
+	if(password != cpassword){
+        errors.push({"text": "Password do not match"});
+    }
+
+    if(password.length < 8){
+        errors.push({"text": "Password must be at least 8 characters"});
+    }
+
+    if(errors.length == 0){
+		bcrypt.genSalt(10, function(err, salt) {
+			bcrypt.hash(password, salt, function(err, hash) {
+				
+			console.log("1");
+        User.update({
+			password: hash
+		},
+		{where:{email: req.body.email}}
+		)
+		.then(user => {
+			if (user > 0) {
+				console.log("3");
+			console.log(user, req.body.email, user.email, errors.length);
+			// If user is found, email has already been registered
+				success_msg = "Password has been resetted";
+				res.render('user/login', {success_msg:success_msg});
+			
+			}else{
+				console.log("2");
+				errors.push({"text": "User not found"});
+				res.render('user/forgetpassword', {errors, email, passport, cpassword});
+			} 
+		})
+	})})
+    }else{
+
+		console.log("4");
+        res.render('user/forgetpassword', {errors, email, password, cpassword})
+    }
+
+});
+
 router.get('/profile', (req,res) => {
 	res.render("user/profile");
-})
+});
 
 
 
