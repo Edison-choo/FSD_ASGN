b6b756f878887ad195cfb4d011e6ffa39c4c075d
Xu4nW31 <joshualim2122@gmail.com>
2021-06-29 20:52:09 +0800

    retrieve reviews

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const ensureAuthenticated = require('../helpers/auth');
+const Reviews = require('../models/reviews');
 
-router.get('/add', (req, res) => {
-    res.render('reviews/reviews');
+router.get('/reviews', (req, res) => {
+    Reviews.findAll(
+        ).then((reviews) => {
+            res.render('reviews/reviews', {reviews: reviews})
+        }).catch(err => console.log(err));
 });
+
 
 module.exports = router;
